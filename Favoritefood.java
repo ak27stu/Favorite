8f1fbe8ad21afe4129f6a55391ca7d38d3902117
ak27stu <75403420+ak27stu@users.noreply.github.com>
2021-04-18 23:34:40 +0900

    Add files via upload

---
+++ Favoritefood.java
@@ -0,0 +1,6 @@
+public class Favoritefood{
+  public static void main(String[]args){
+    System.out.println("私はマシュマロが好きです。");
+    System.out.println("なぜなら白くて丸いフォルムがかわいい上に、焼いたりケーキにしたりなど活用方法が多くて楽しいからです。");
+  }
+}
